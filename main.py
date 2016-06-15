f3c7bd42679958cb5a82511dff637c0dc781b0b4
Teodoro Fields Collin <teocollin1995@gmail.com>
2016-06-15 19:01:28 -0400

    crap

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-"""`main` is the top level module for your Flask application."""
-
+import json
 # Import the Flask Framework
 from flask import Flask
 app = Flask(__name__)
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
-
+token = "230588661:AAHxRwP4fH473Sva_XQqF8IAvJGfFtqRH_o"
+@app.route('/setwh')
+def set_webhook():
+    """Exists to setup the webhook from the telegram api to the appengine site"""
+    urlfetch.set_default_fetch_deadline(60)
+    return json.dumps(json.load(urllib2.urlopen(BASE_URL + 'setWebhook', urllib.urlencode({'url': "https://rembot-1343.appspot.com/webhook"}))))
 
 @app.route('/')
 def hello():
