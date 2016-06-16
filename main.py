108126056f88daaa6e14cb4b50e30587aa8f46bb
Teodoro Fields Collin <teocollin1995@gmail.com>
2016-06-16 00:28:31 -0400

    date time

--- main.py
+++ main.py
@@ -4,16 +4,22 @@ import urllib
 import urllib2
 from flask import Flask, Response, request
 from google.appengine.api import urlfetch, memcache
+from google.appengine.ext import ndb
 app = Flask(__name__)
-# Note: We don't need to call run() since our application is embedded within
-# the App Engine WSGI application server.
+
+#constant for connecting to telegram
 TOKEN = "230588661:AAHxRwP4fH473Sva_XQqF8IAvJGfFtqRH_o"
 BASE_URL = 'https://api.telegram.org/bot' + TOKEN + '/'
-@app.route('/setwh')
-def set_webhook():
-    """Exists to setup the webhook from the telegram api to the appengine site"""
-    urlfetch.set_default_fetch_deadline(60)
-    return json.dumps(json.load(urllib2.urlopen(BASE_URL + 'setWebhook', urllib.urlencode({'url': "https://rembot-1343.appspot.com/webhook"}))))
+
+
+#ndd records 
+
+class CronJob(ndb.Model):
+    time = ndb.DateTimeProperty()
+    chat_id = ndb.StringProperty()
+    message_id = ndb.StringProperty()
+    msg = ndb.StringProperty()
+
 def give_response(chat_id, message_id, msg):
     resp = urllib2.urlopen(BASE_URL + 'sendMessage', urllib.urlencode({
         'chat_id': str(chat_id),
@@ -22,6 +28,16 @@ def give_response(chat_id, message_id, msg):
         'reply_to_message_id': str(message_id),
     })).read()
     return(resp)
+
+
+
+
+@app.route('/setwh')
+def set_webhook():
+    """Exists to setup the webhook from the telegram api to the appengine site"""
+    urlfetch.set_default_fetch_deadline(60)
+    return json.dumps(json.load(urllib2.urlopen(BASE_URL + 'setWebhook', urllib.urlencode({'url': "https://rembot-1343.appspot.com/webhook"}))))
+
 
 
 @app.route('/webhook', methods=["PUT", "POST"])
